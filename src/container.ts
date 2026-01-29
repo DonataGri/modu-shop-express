b043d94300078dcf9fc3c4beff2c9027a5c31e7a
DonataG <grigalionyte.donata@gmail.com>
2026-01-29 14:40:37 +0100

    chore: remove console logs

--- src/container.ts
+++ src/container.ts
@@ -5,7 +5,6 @@ import { createProductRoutes } from "./features/products/products.routes";
 import { ProductService } from "./features/products/products.service";
 import { PrismaClient } from "../generated/prisma/client";
 
-console.log(process.env.DATABASE_URL, "URL");
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
